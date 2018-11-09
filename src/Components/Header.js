0f9dbbf56d14e5d9dd504eec81da54d40cee8eaa
keschaefer <keschaefer@gmail.com>
2018-11-09 11:34:29 -0700

    added basic header content, imported to app.js

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+      <div>
+         <h1>DENVER DOGGIE</h1>
+         <h3>Take your pup with you!</h3>
+         <p>Do you love your pup so much that you want to take them with you everywhere you go? Of course you do! Now you can use Denver Doggie to search for all kinds of places where your pup is welcome, restaurants, bars, stores, and more!</p>
+      </div>
+   )
+}
+
+export default Header
